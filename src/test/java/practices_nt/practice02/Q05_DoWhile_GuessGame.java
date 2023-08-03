82ea4cfb6f0357650baed8913e6137f4c3f313c0
Saima <saima@Syeds-iMac.local>
2023-08-03 06:37:37 +0400

    nt-practice03

--- src/test/java/practices_nt/practice02/Q05_DoWhile_GuessGame.java
+++ src/test/java/practices_nt/practice02/Q05_DoWhile_GuessGame.java
@@ -1,5 +1,6 @@
 package practices_nt.practice02;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Q05_DoWhile_GuessGame {
@@ -15,14 +16,55 @@ public class Q05_DoWhile_GuessGame {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // System.out.println(Math.random()*100);  // returns any number between 0 and 100 where 100 is exclusive
-        // System.out.println((int) (Math.random()*101));  // returns any number between 0 and 100 where 100 is inclusive
+         // System.out.println(Math.random()*100);  // returns any number between 0 and 100 where 100 is exclusive
+        //  System.out.println((int) (Math.random()*101));  // returns any number between 0 and 100 where 100 is inclusive
 
         int randomNumber = (int) (Math.random()*101);
+        System.out.println("randomNumber = " + randomNumber);
 
+//        Random random = new Random();
+//        int randomNum = random.nextInt(101);
+//        System.out.println(randomNum);
 
-        // TO BE CONTINUED!!!!
+        System.out.println("Enter a number");
 
+        int number;
+        int numOfAttempts = 10;
+
+        do{
+            numOfAttempts--;
+             number = scan.nextInt();
+
+            if (number<randomNumber){
+                System.out.println("Enter a greater number");
+                System.out.println("You have "+numOfAttempts+" attempt(s) left!");
+
+            }else if(number>randomNumber){
+                System.out.println("Enter a smaller number");
+                System.out.println("You have "+numOfAttempts+" attempt(s) left!");
+            }else {
+                System.out.println("Congratulations! You found the number!");
+                System.out.println("You have guessed the number in "+(10-numOfAttempts)+" attempt(s)");
+                System.out.println("You scored "+ (numOfAttempts +1)*10 +" points");
+                break;
+            }
+
+            // in case, user can't guess the number in 10 attempts
+            if (numOfAttempts==0){
+                System.out.println("You failed! You could not guess the number!!");
+                System.out.println("Continue: Y/N");
+                String condition = scan.next();
+                System.out.println("Enter a number");
+                if (condition.equalsIgnoreCase("y")){
+                    numOfAttempts = 10;
+                    randomNumber = (int) (Math.random()*101);
+                }else {
+                    break;
+                }
+            }
+
+        }while (true);
+        System.out.println("Bye! Thanks for playing");
 
 
     }
---
+++ src/test/java/practices_nt/practice03/Q01_Ascii_OneToHundred.java
@@ -0,0 +1,18 @@
+package practices_nt.practice03;
+
+public class Q01_Ascii_OneToHundred {
+    /*
+    Print on the console numbers from 1 to 100.
+    But do not use any number in your code
+     */
+    public static void main(String[] args) {
+
+//        System.out.println('d'/ 'd');
+//        System.out.println((int)'d');
+
+        for (int i ='d'/ 'd' ; i <='d' ; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
